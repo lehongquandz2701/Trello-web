0c533454537ab4ed61c931fd63c1b96afc639b35
Simon <lehongquandz2701@gmail.com>
2025-03-02 18:20:21 +0700

    feat: update list

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -5,49 +5,70 @@ type TListProps = {
   data?: any;
 };
 
-const List = ({ content, data }: TListProps) => (
-  <>
-    <style>
-      {`
-        .custom-virtuoso::-webkit-scrollbar {
-         width: 14px; 
-        height: 14px;
-        }
-        .custom-virtuoso::-webkit-scrollbar-thumb {
-          background: #313A39;
-          border-radius: 10px;
-            cursor: pointer;
-            border: 2px solid transparent; 
-             background-clip: padding-box;
-        }
-    
-        .custom-virtuoso {
-          height: calc(100vh - 130px);
-          width: 100%;
-          overflow: auto;
+const List = ({ content, data }: TListProps) => {
+  function Footer() {
+    return (
+      <div
+        style={{
+          padding: "10px",
+          display: "inline-block",
+          whiteSpace: "nowrap",
+          verticalAlign: "top",
+          background: "Red",
+        }}
+      >
+        end reached{" "}
+      </div>
+    );
+  }
+
+  return (
+    <>
+      <style>
+        {`
+          .custom-virtuoso::-webkit-scrollbar {
+           width: 14px; 
+          height: 14px;
+          }
+          .custom-virtuoso::-webkit-scrollbar-thumb {
+            background: #313A39;
+            border-radius: 10px;
+              cursor: pointer;
+              border: 2px solid transparent; 
+               background-clip: padding-box;
+          }
       
-        }
-      `}
-    </style>
-    <Virtuoso
-      className="custom-virtuoso"
-      horizontalDirection
-      computeItemKey={(index, item) => index}
-      data={[1, 2, 3, 4]}
-      itemContent={() => (
-        <div
-          style={{
-            padding: "10px",
-            display: "flex",
-            justifyContent: "flex-start",
-            alignItems: "flex-start",
-          }}
-        >
-          {content}
-        </div>
-      )}
-    />
-  </>
-);
+          .custom-virtuoso {
+            height: calc(100vh - 130px);
+            width: 100%;
+            overflow: auto;
+        
+          }
+        `}
+      </style>
+      <Virtuoso
+        components={{
+          Header: Footer,
+        }}
+        className="custom-virtuoso"
+        horizontalDirection
+        computeItemKey={(index, item) => index}
+        data={[1, 2, 3, 4]}
+        itemContent={() => (
+          <div
+            style={{
+              padding: "10px",
+              display: "flex",
+              justifyContent: "flex-start",
+              alignItems: "flex-start",
+            }}
+          >
+            {content}
+          </div>
+        )}
+      />
+    </>
+  );
+};
 
 export default List;
--- src/pages/Boards/BoardContent/index.tsx
+++ src/pages/Boards/BoardContent/index.tsx
@@ -1,5 +1,7 @@
-import { BoardItem, List } from "~/components";
+import { BoardItem } from "~/components";
 import background from "~/assets/image/bg.png";
+import { Box, Button } from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
 const BoardContent = () => {
   return (
     <>
@@ -16,7 +18,64 @@ const BoardContent = () => {
         alt="background"
         loading="lazy"
       />
-      <List content={<BoardItem />} />
+
+      <Box
+        sx={{
+          height: "calc(100vh - 130px)",
+          width: "100%",
+          overflow: "auto",
+          "&::-webkit-scrollbar": {
+            width: "10px",
+          },
+          cursor: "pointer",
+
+          "&::-webkit-scrollbar-thumb": {
+            background: "#313A39",
+            borderRadius: "4px",
+          },
+        }}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "flex-start",
+            alignItems: "flex-start",
+          }}
+        >
+          {[1, 2, 3, 4, 45, , 6, 7, 8, 0, 9, 1212, 31].map((item, index) => (
+            <Box
+              key={index}
+              sx={{
+                padding: "10px",
+              }}
+            >
+              <BoardItem />
+            </Box>
+          ))}
+
+          <Box
+            sx={{
+              padding: "10px",
+            }}
+          >
+            <Button
+              startIcon={<AddIcon />}
+              sx={{
+                width: "200px",
+                backgroundColor: (theme) =>
+                  theme.palette.mode === "dark" ? "black" : "#b9d3d0",
+                height: "44px",
+                color: (theme) =>
+                  theme.palette.mode === "dark" ? "white" : "black",
+                justifyContent: "flex-start",
+                padding: 2,
+              }}
+            >
+              Add another list
+            </Button>
+          </Box>
+        </Box>
+      </Box>
     </>
   );
 };
