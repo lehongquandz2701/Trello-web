d79ebcdf95aa57b32aeefd8f4e1b69c6be0faf74
Simon <lehongquandz2701@gmail.com>
2025-03-16 16:40:30 +0700

    feat: update dnd

--- src/components/BoardItem.tsx
+++ src/components/BoardItem.tsx
@@ -3,31 +3,33 @@ import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
 import { useCallback, useMemo, useState } from "react";
 import AddIcon from "@mui/icons-material/Add";
 import TextareaAutosize from "react-textarea-autosize";
-import MediaCard from "./MediaCard";
 import { TColumns } from "~/utilities/types";
-import { mapOrder } from "~/hepler";
 import { useSortable } from "@dnd-kit/sortable";
 import { CSS } from "@dnd-kit/utilities";
+import ListCard from "~/pages/Boards/BoardContent/ListCard";
 
 type TBoardItemProps = {
   item: TColumns;
 };
 
 const BoardItem = ({ item }: TBoardItemProps) => {
-  const { attributes, listeners, setNodeRef, transform, transition } =
-    useSortable({ id: item._id, data: { ...item } });
+  const {
+    attributes,
+    listeners,
+    setNodeRef,
+    transform,
+    transition,
+    isDragging,
+  } = useSortable({ id: item._id, data: { ...item } });
 
   const dndKitColumnStyle = {
     transform: CSS.Translate.toString(transform),
     transition,
-    touchAction: "none",
+    height: "100%",
+    opacity: isDragging ? 0.5 : undefined,
   };
 
   const [isFocus, setFocus] = useState(false);
-
-  const cardArray = useMemo(() => {
-    return mapOrder(item.cards, item.cardOrderIds, "_id");
-  }, [item]);
 
   const styleInput = useMemo(() => {
     return isFocus
@@ -40,134 +42,105 @@ const BoardItem = ({ item }: TBoardItemProps) => {
   }, [setFocus]);
 
   return (
-    <Box
-      {...listeners}
-      {...attributes}
-      style={dndKitColumnStyle}
-      ref={setNodeRef}
-      sx={{
-        padding: "10px",
-      }}
-    >
+    <div {...attributes} style={dndKitColumnStyle} ref={setNodeRef}>
       <Box
+        {...listeners}
         sx={{
-          width: "272px",
-          bgcolor: (theme) =>
-            theme.palette.mode === "dark" ? "black" : "#b9d3d0",
-          borderRadius: "10px",
-          padding: 1,
-          height: "fit-content",
+          padding: "10px",
         }}
       >
         <Box
           sx={{
-            width: "100%",
-            alignItems: "center",
-            justifyContent: "center",
-            display: "flex",
-            position: "relative",
-            marginBottom: "10px",
-            height: 40,
-          }}
-        >
-          <TextareaAutosize
-            value={item.title}
-            maxRows={15}
-            onFocus={onFocusAndBlur}
-            onBlur={onFocusAndBlur}
-            style={{
-              marginRight: "50px",
-              height: 34,
-              width: "100%",
-              overflow: "hidden",
-              outline: "none",
-              padding: "5px 10px",
-              fontSize: "16px",
-              fontWeight: "500",
-              borderRadius: 5,
-              resize: "none",
-              ...styleInput,
-            }}
-          />
-
-          <IconButton
-            sx={{
-              position: "absolute",
-              top: 0,
-              right: 5,
-              ":hover": {
-                bgcolor: "#393b3d",
-                borderRadius: 2,
-              },
-            }}
-            aria-label="add an alarm"
-          >
-            <MoreHorizIcon />
-          </IconButton>
-        </Box>
-        <Box
-          sx={{
-            p: "0 5px",
-            maxHeight: "calc(100vh - 280px)",
-            overflowX: "hidden",
-            overflowY: "auto",
-            "&::-webkit-scrollbar": {
-              width: "10px",
-            },
-            cursor: "pointer",
-            // #313A39 #ced0da
-            "&::-webkit-scrollbar-thumb": {
-              background: "#313A39",
-              borderRadius: "4px",
-            },
+            width: "272px",
+            bgcolor: (theme) =>
+              theme.palette.mode === "dark" ? "black" : "#b9d3d0",
+            borderRadius: "10px",
+            padding: 1,
+            height: "fit-content",
           }}
         >
           <Box
             sx={{
-              cursor: "pointer",
-              flexDirection: "column",
+              width: "100%",
+              alignItems: "center",
+              justifyContent: "center",
               display: "flex",
-              gap: 1,
+              position: "relative",
+              marginBottom: "10px",
+              height: 40,
             }}
           >
-            {cardArray.map((data) => (
-              <MediaCard key={data._id} data={data} />
-            ))}
+            <TextareaAutosize
+              value={item.title}
+              maxRows={15}
+              onFocus={onFocusAndBlur}
+              onBlur={onFocusAndBlur}
+              style={{
+                marginRight: "50px",
+                height: 34,
+                width: "100%",
+                overflow: "hidden",
+                outline: "none",
+                padding: "5px 10px",
+                fontSize: "16px",
+                fontWeight: "500",
+                borderRadius: 5,
+                resize: "none",
+                ...styleInput,
+              }}
+            />
+
+            <IconButton
+              sx={{
+                position: "absolute",
+                top: 0,
+                right: 5,
+                ":hover": {
+                  bgcolor: "#393b3d",
+                  borderRadius: 2,
+                },
+              }}
+              aria-label="add an alarm"
+            >
+              <MoreHorizIcon />
+            </IconButton>
+          </Box>
+
+          <ListCard cards={item.cards} cardOrderIds={item.cardOrderIds} />
+
+          <Box
+            sx={{
+              width: "100%",
+              height: 40,
+            }}
+          >
+            <Button
+              startIcon={
+                <AddIcon
+                  sx={{
+                    height: "20px",
+                    width: "20px",
+                  }}
+                />
+              }
+              sx={{
+                width: "85%",
+                justifyContent: "flex-start",
+                ":hover": {
+                  bgcolor: "#393b3d",
+                  borderRadius: 2,
+                },
+                color: (theme) =>
+                  theme.palette.mode === "dark" ? "white" : "black",
+                fontSize: "16px",
+              }}
+            >
+              Add a card
+            </Button>
           </Box>
         </Box>
-
-        <Box
-          sx={{
-            width: "100%",
-            height: 40,
-          }}
-        >
-          <Button
-            startIcon={
-              <AddIcon
-                sx={{
-                  height: "20px",
-                  width: "20px",
-                }}
-              />
-            }
-            sx={{
-              width: "85%",
-              justifyContent: "flex-start",
-              ":hover": {
-                bgcolor: "#393b3d",
-                borderRadius: 2,
-              },
-              color: (theme) =>
-                theme.palette.mode === "dark" ? "white" : "black",
-              fontSize: "16px",
-            }}
-          >
-            Add a card
-          </Button>
-        </Box>
       </Box>
-    </Box>
+    </div>
   );
 };
 
